5995627b546839b857c8fcdbbd893c7a3e69d2b1
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-08-21 18:56:05 -0300

    Subindo o projeto springboot

---
+++ src/main/java/com/websocket/project/WebsocketProjectApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketProjectApplication.class, args);
+	}
+
+}
