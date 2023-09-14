cd02a3a330fd7f8d17d4a0c82d6ea71f42273f0a
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-09-14 19:34:16 -0300

    controller

---
+++ src/main/java/com/websocket/project/controller/ControllerBroadcast.java
@@ -0,0 +1,21 @@
+package com.websocket.project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerBroadcast {
+	
+	@Autowired
+	private SimpMessagingTemplate simpMessagingTemplate;
+
+	@MessageMapping("/news")
+	public void broadcastNews(@Payload String message) {
+		
+		this.simpMessagingTemplate.convertAndSend("/topic/news", message);
+	}
+
+}
