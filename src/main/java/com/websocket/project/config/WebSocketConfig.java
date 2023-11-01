8c4e143c7ba586a497ca910a333ff845d1741fb3
Fabricio Vieira <bvieira.fabricio@gmail.com>
2023-11-01 19:44:23 -0300

    Alterando a classe WebSocketConfig

--- src/main/java/com/websocket/project/config/WebSocketConfig.java
+++ src/main/java/com/websocket/project/config/WebSocketConfig.java
@@ -1,36 +1,24 @@
 package com.websocket.project.config;
 
-import com.websocket.project.CustomStompSessionHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-	
+
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/mywebsockets")
 				.setAllowedOrigins("localhost:8080/mywebsockets").withSockJS();
 	}
-	
+
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
 		config.enableSimpleBroker("/topic/", "/queue/");
 		config.setApplicationDestinationPrefixes("/app");
 	}
-	
-	
-	/*@Autowired
-	private WebSocketStompClient stompClient;
-	
-	StompSessionHandler sessionHandler = new CustomStompSessionHandler();
-	StompSession stompSession = stompClient.connect(loggerServerQueueUrl, sessionHandler).get();*/
 }
